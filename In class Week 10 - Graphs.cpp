945d4963edd7a6b2773354b2573a5ebdd09bd5e0
HelithaNimnaka <109774252+HelithaNimnaka@users.noreply.github.com>
2023-05-16 17:58:47 +0530

    Update In class Week 10 - Graphs.cpp

--- In class Week 10 - Graphs.cpp
+++ In class Week 10 - Graphs.cpp
@@ -20,7 +20,7 @@ struct Graph{
         }
     }
 
-    void addEdge(int u, int v) {
+    void addedge(int u, int v) {
         nodes[u].neighbours.push_back(v);
         nodes[v].neighbours.push_back(u);
     }
@@ -41,18 +41,18 @@ int main() {
     g->intializenodes();
 
     // Add edges to the graph
-    g->addEdge(1, 2);
-    g->addEdge(1, 4);
-    g->addEdge(1, 3);
-    g->addEdge(1, 5);
-    g->addEdge(2, 3);
-    g->addEdge(2, 6);
-    g->addEdge(4, 6);
-    g->addEdge(4, 7);
-    g->addEdge(4, 8);
-    g->addEdge(5, 7);
-    g->addEdge(5, 8);
-    g->addEdge(5, 6);
+    g->addedge(1, 2);
+    g->addedge(1, 4);
+    g->addedge(1, 3);
+    g->addedge(1, 5);
+    g->addedge(2, 3);
+    g->addedge(2, 6);
+    g->addedge(4, 6);
+    g->addedge(4, 7);
+    g->addedge(4, 8);
+    g->addedge(5, 7);
+    g->addedge(5, 8);
+    g->addedge(5, 6);
     // Print the graph adjacency list
     g->print();
 
